f126c5c71658f7bcbb98cd3ac3631690a9d9869d
maharjansanam027 <108982463+maharjansanam027@users.noreply.github.com>
2024-12-11 22:15:26 +0545

    practice about the Math object in js

---
+++ JS/Day06/math.js
@@ -0,0 +1,57 @@
+// let num1 = 223;
+// let num2 = new Number(223);
+// let num3 = new Number(223);
+
+// console.log(num1==num2);
+
+// console.log(num2== num3);
+
+let num1 = 231.78;
+
+// it will round off the float value after point
+console.log(num1.toFixed(1));
+
+
+// it will round off to the number before number
+console.log(num1.toPrecision(3));
+
+
+console.log(num1.valueOf());
+
+
+console.log("............................");
+
+// mathmatical culation object
+
+// console.log(Math.PI);
+
+
+// it will generate number 0<=num<1
+// console.log(Math.random());
+
+// floor and ceil
+
+
+// let num = 23.3;
+
+// it will take first number number before decimal
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+
+
+// generate 0 to 9
+// console.log(Math.floor(Math.random()*10));
+
+
+// generate 11 to 20
+// console.log(Math.floor(Math.random()*10)+11);
+
+
+// formula to generaate number 
+
+// console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+// generate 40 to 50;
+
+console.log(Math.floor(Math.random()*(50-40+1)+40));
+
