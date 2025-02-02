1f2d689add5feec2bf520c34297d040612809caf
Sanam Maharjan <maharjansanam027@gmail.com>
2025-02-02 17:40:52 +0545

    all files

---
+++ Github3/index.js
@@ -0,0 +1,18 @@
+const button = "Added the button"
+console.log(button);
+
+const login = "Login page added";
+console.log(login);
+
+const footer = "footer added"
+console.log(footer);
+const payment = "Payment added"
+console.log(payment);
+
+const upi = "upi added"
+
+
+
+// Going to fixex bug
+console.log("Bug Fixed");
+
